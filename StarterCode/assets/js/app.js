c1a642f181d5ec55bcd694ba5eac95000b2f5d11
akt2 <atwitchellheyne@gmail.com>
2019-06-11 18:58:49 -0400

    more

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,1 +1,42 @@
 // @TODO: YOUR CODE HERE!
+function makeResponsive() {
+    var svgArea=d3.select('body').select('svg');
+    if (!svgArea.empty()) {
+        svgArea.remove();
+    }
+};
+
+var width=window.innerWidth-100;
+var height=window.innerHeight-100;
+
+var svg=d3.select('.scatter').append('svg').attr('width',window.innerWidth)
+    .attr('height',window.innerHeight);
+
+var group=svg.append('g').attr('transform',`translate(${margin.left}, ${margin.top})`);
+
+d3.csv('../data/data.csv')
+    .then(function(povdata) {
+        povdata.forEach(function(data) {
+            data.income=+data.income;
+            data.smokes=+data.smokes;
+        });
+
+        var xscale=d3.scaleLinear().domain([0,d3.max(povdata,d=>d.income)])
+        .range([0,width]);
+        var yscale=d3.scaleLinear()
+        .domain([0,d3.max(povdata,d=>d.smokes)]).range([height,0]);
+        var xaxis=d3.axisBottom(xscale);
+        var yaxis=d3.axisLeft(yscale);
+
+        group.append('g').attr('transform',`translate(0, ${height})`).call(xaxis);
+        group.append('g').call(yaxis);
+
+        var circs=group.selectAll('circle').data(povdata).enter().append('circle')
+            .attr('circx',d=>xLinearScale(d.income)).attr('circy',d=>yLinearScale(d.smokes))
+            .attr('r','10').attr('fill','blue');
+        
+        var toolTip=d3.tip().attr('class','tooltip').offset([80,-60]).html(function(d) {
+            return(`${d.abbr}<br>Income: $${d.income}<br>Smokers: ${d.smokes}%`);
+        });
+    
+    })
